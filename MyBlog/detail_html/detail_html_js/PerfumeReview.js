4ca9c3bd0a9c25a54ab26d6792ff0b14b423e07e
Kim Jinsoo <kjs981021@gmail.com>
2025-02-09 07:41:48 +0900

    Update PerfumeReview.js

--- MyBlog/detail_html/detail_html_js/PerfumeReview.js
+++ MyBlog/detail_html/detail_html_js/PerfumeReview.js
@@ -1,19 +1,19 @@
 document.getElementById('spring').addEventListener('click', function() {
     console.log('봄 향수 후기');
-    window.location.href = 'spring.html';
+    window.location.href = '/MyBlog/detail_html/spring.html';
   });
   
   document.getElementById('summer').addEventListener('click', function() {
     console.log('여름 향수 후기');
-    window.location.href = 'summer.html';
+    window.location.href = '/MyBlog/detail_html/summer.html';
   });
   
   document.getElementById('autumn').addEventListener('click', function() {
     console.log('가을 향수 후기');
-    window.location.href = 'autumn.html';
+    window.location.href = '/MyBlog/detail_html/autumn.html';
   });
   
   document.getElementById('winter').addEventListener('click', function() {
     console.log('겨울 향수 후기');
-    window.location.href = 'winter.html';
+    window.location.href = '/MyBlog/detail_html/winter.html';
   });
